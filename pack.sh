5a07f087ae2d17bb071cad0291e4170385e246bd
Poulpi41 <thomann.raphael41@gmail.com>
2023-11-19 19:58:20 +0100

    pack script

---
+++ pack.sh
@@ -0,0 +1,6 @@
+npx webpack --config ./webpack.config.js
+mkdir -p ./synthListSite
+mkdir -p ./synthListSite/js
+mkdir -p ./synthListSite/css
+cp ./index.html ./synthListSite
+cp ./js/main.js ./synthListSite/js
